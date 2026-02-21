ff48b53f209574f1a89a4608b589ba1f95e95f71
tanvi <tanvigarg.nj@gmail.com>
2026-02-21 17:43:12 -0500

    login routes, options for signup

--- my-app/src/Login.tsx
+++ my-app/src/Login.tsx
@@ -1,10 +1,30 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+//import Home from "./Home.tsx";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+const handleLogin = async () => {
+    console.log("login button clicker");
+  const res = await fetch("http://localhost:5000/api/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+  });
 
+  if (!res.ok) {
+    alert("Invalid username or password");
+    return;
+  }
+
+  console.log("RESPONSE STATUS:", res.status);
+  const user = await res.json();
+  console.log("Logged in user:", user);
+  //take user to home (which we defined as /)
+   navigate("/");
+};
   return (
     <div style={{ padding: 20 }}>
       <h2>Login</h2>
@@ -24,10 +44,10 @@ function Login() {
       />
       <br /><br />
 
-      <button>Login</button>
+      <button type = "button" onClick={handleLogin}>Login</button>
 
       <p style={{ marginTop: 20 }}>
-        Don’t have an account? <Link to="/signup">Create Account</Link>
+        Don't have an account? <Link to="/signup">Create Account</Link>
       </p>
     </div>
   );
